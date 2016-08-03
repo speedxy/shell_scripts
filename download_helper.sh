1d79b337082fcb1ab97f0d4098bd983527b3f665
speedxy <swat@parrot-media.de>
2016-08-03 18:14:17 +0200

    Update download_helper.sh

--- download_helper.sh
+++ download_helper.sh
@@ -25,22 +25,4 @@ REMOTE_MYSQL_USER="$REMOTE_SSH_USER"
 LOCAL_MYSQL_HOST="localhost"
 LOCAL_DIR="$SCRIPT_DIR/../$LOC_DIR"
 
-$SCRIPT_DIR/download.sh $REMOTE_HOST $REMOTE_SSH_USER $REMOTE_MYSQL_HOST $REMOTE_MYSQL_USER $REMOTE_MYSQL_PASS $REMOTE_MYSQL_DB $REMOTE_DIR $LOCAL_DIR $CMS
-
-# WordPress
-#sed -i "s/'DB_NAME', '$REMOTE_MYSQL_DB'/'DB_NAME', '$LOCAL_MYSQL_DB'/g" $LOCAL_DIR/wp-config.php
-#sed -i "s/'DB_USER', '$REMOTE_MYSQL_USER'/'DB_USER', '$LOCAL_MYSQL_USER'/g" $LOCAL_DIR/wp-config.php
-#sed -i "s/'DB_PASSWORD', '$REMOTE_MYSQL_PASS'/'DB_PASSWORD', '$LOCAL_MYSQL_PASS'/g" $LOCAL_DIR/wp-config.php
-#sed -i "s/'DB_HOST', '$REMOTE_MYSQL_HOST'/'DB_HOST', '$LOCAL_MYSQL_HOST'/g" $LOCAL_DIR/wp-config.php
-
-# PM CMS
-#sed -i "s/host = \"$REMOTE_MYSQL_HOST\"/host = \"$LOCAL_MYSQL_HOST\"/g" $LOCAL_DIR/ini/host.ini
-#sed -i "s/database = \"$REMOTE_MYSQL_DB\"/database = \"$LOCAL_MYSQL_DB\"/g" $LOCAL_DIR/ini/host.ini
-#sed -i "s/username = \"$REMOTE_MYSQL_USER\"/username = \"$LOCAL_MYSQL_USER\"/g" $LOCAL_DIR/ini/host.ini
-#sed -i "s/password = \"$REMOTE_MYSQL_PASS\"/password = \"$LOCAL_MYSQL_PASS\"/g" $LOCAL_DIR/ini/host.ini
-
-# TYPO3
-#sed -i "s/'password' => '$REMOTE_MYSQL_PASS'/'password' => '$LOCAL_MYSQL_PASS'/" $LOCAL_DIR/typo3conf/LocalConfiguration.php
-#sed -i "s/'username' => '$REMOTE_MYSQL_USER'/'username' => '$LOCAL_MYSQL_USER'/" $LOCAL_DIR/typo3conf/LocalConfiguration.php
-#sed -i "s/'database' => '$REMOTE_MYSQL_DB'/'database' => '$LOCAL_MYSQL_DB'/" $LOCAL_DIR/typo3conf/LocalConfiguration.php
-#sed -i "s/'host' => '$REMOTE_MYSQL_HOST'/'host' => '$LOCAL_MYSQL_HOST'/" $LOCAL_DIR/typo3conf/LocalConfiguration.php
+$SCRIPT_DIR/download.sh $REMOTE_HOST $REMOTE_SSH_USER $REMOTE_MYSQL_HOST $REMOTE_MYSQL_USER $REMOTE_MYSQL_PASS $REMOTE_MYSQL_DB $REMOTE_DIR $LOCAL_MYSQL_HOST $LOCAL_MYSQL_USER $LOCAL_MYSQL_PASS $LOCAL_MYSQL_DB $LOCAL_DIR $CMS
