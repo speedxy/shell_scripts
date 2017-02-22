a55c929b30db67368ac726ec7b6ba4ea37170e1b
speedxy <swat@parrot-media.de>
2017-02-22 12:20:27 +0100

    Update download.sh
    xt:commerce: root directory is now updated
    new parameter mysqldump_conf
    local_dir is now modified via realpath for absolute paths (neede for xt:commerce root directory configuration)

--- download.sh
+++ download.sh
@@ -15,8 +15,9 @@
 
 # Prüfe Parameter
 if [ $# = 0 ] ; then # Keine Argumente: Starte interaktiven Modus
-    echo "Usage: $0 remote_host remote_ssh_user remote_mysql_host remote_mysql_user remote_mysql_pass remote_mysql_db remote_dir local_mysql_host local_mysql_user local_mysql_pass local_mysql_db local_dir cms"
+    echo "Usage: $0 remote_host remote_ssh_user remote_mysql_host remote_mysql_user remote_mysql_pass remote_mysql_db remote_dir local_mysql_host local_mysql_user local_mysql_pass local_mysql_db local_dir cms mysqldump_conf"
     echo "cms (wordpress|pmcms|typo3-6)"
+    echo "mysqldump_conf (--skip-set-charset)"
     exit 65
   else # Alle Parameter übergeben
     # Variablen initialisieren (Remote)
@@ -33,27 +34,30 @@ if [ $# = 0 ] ; then # Keine Argumente: Starte interaktiven Modus
     LOCAL_MYSQL_USER=$9
     LOCAL_MYSQL_PASS=${10}
     LOCAL_MYSQL_DB=${11}
-    LOCAL_DIR=${12}
-    
+    LOCAL_DIR=$(realpath "${12}")
+
     CMS=${13}
+
+    MYSQLDUMP_CONF=${14}
 fi
+
 
 # Parameter-Zusammenfassung (debugging)
 echo "Synchronisiere Dateisystem: $REMOTE_SSH_USER@$REMOTE_HOST:$REMOTE_DIR => $LOCAL_DIR"
-echo "Synchronisiere Datenbank: $REMOTE_MYSQL_USER@$REMOTE_MYSQL_HOST.$REMOTE_MYSQL_DB => $LOCAL_MYSQL_USER@$LOCAL_MYSQL_HOST.$LOCAL_MYSQL_DB"
+echo "Synchronisiere Datenbank: $REMOTE_MYSQL_USER@$REMOTE_MYSQL_HOST.$REMOTE_MYSQL_DB on $REMOTE_HOST => $LOCAL_MYSQL_USER@$LOCAL_MYSQL_HOST.$LOCAL_MYSQL_DB on localhost"
 
 # Synchronisiere Dateien
 echo "Synchronisiere Dateien..."
-echo "rsync -rltchvzP -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --delete ${REMOTE_SSH_USER}@${REMOTE_HOST}:${REMOTE_DIR} ${LOCAL_DIR}"
+echo "rsync -rltchvzP -e \"ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null\" --delete ${REMOTE_SSH_USER}@${REMOTE_HOST}:${REMOTE_DIR} \"${LOCAL_DIR}/\""
 echo "Bitte SSH-Kennwort eingeben:"
-rsync -rltchvzP -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --delete ${REMOTE_SSH_USER}@${REMOTE_HOST}:${REMOTE_DIR} ${LOCAL_DIR}
+rsync -rltchvzP -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --delete ${REMOTE_SSH_USER}@${REMOTE_HOST}:${REMOTE_DIR} "${LOCAL_DIR}/"
 echo "Fertig."
 
 # Synchronisiere Datenbank
 echo "Synchronisiere Datenbank..."
-echo "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=quiet ${REMOTE_SSH_USER}@${REMOTE_HOST} "mysqldump -h${REMOTE_MYSQL_HOST} -u${REMOTE_MYSQL_USER} -p*** ${REMOTE_MYSQL_DB}" | mysql -h${LOCAL_MYSQL_HOST} -u$LOCAL_MYSQL_USER -p*** $LOCAL_MYSQL_DB"
+echo "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=quiet ${REMOTE_SSH_USER}@${REMOTE_HOST} \"mysqldump -h${REMOTE_MYSQL_HOST} -u${REMOTE_MYSQL_USER} -p***** ${REMOTE_MYSQL_DB}\" | mysql -h${LOCAL_MYSQL_HOST} -u$LOCAL_MYSQL_USER -p***** $LOCAL_MYSQL_DB"
 echo "Bitte SSH-Kennwort erneut eingeben:"
-ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=quiet ${REMOTE_SSH_USER}@${REMOTE_HOST} "mysqldump -h${REMOTE_MYSQL_HOST} -u${REMOTE_MYSQL_USER} -p${REMOTE_MYSQL_PASS} ${REMOTE_MYSQL_DB}" | mysql -h${LOCAL_MYSQL_HOST} -u$LOCAL_MYSQL_USER -p$LOCAL_MYSQL_PASS $LOCAL_MYSQL_DB
+ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -o LogLevel=quiet ${REMOTE_SSH_USER}@${REMOTE_HOST} "mysqldump -h${REMOTE_MYSQL_HOST} -u${REMOTE_MYSQL_USER} -p${REMOTE_MYSQL_PASS} {$MYSQLDUMP_CONF} ${REMOTE_MYSQL_DB}" | mysql -h${LOCAL_MYSQL_HOST} -u$LOCAL_MYSQL_USER -p$LOCAL_MYSQL_PASS $LOCAL_MYSQL_DB
 echo "Fertig."
 
 case "$CMS" in
@@ -95,7 +99,6 @@ case "$CMS" in
     sed -i "s/define('_SYSTEM_DATABASE_USER', '$REMOTE_MYSQL_USER');/define('_SYSTEM_DATABASE_USER', '$LOCAL_MYSQL_USER');/" $LOCAL_DIR/conf/config.php
     sed -i "s/define('_SYSTEM_DATABASE_DATABASE', '$REMOTE_MYSQL_DB');/define('_SYSTEM_DATABASE_DATABASE', '$LOCAL_MYSQL_DB');/" $LOCAL_DIR/conf/config.php
     sed -i "s/define('_SYSTEM_DATABASE_HOST', '$REMOTE_MYSQL_HOST');/define('_SYSTEM_DATABASE_HOST', '$LOCAL_MYSQL_HOST');/" $LOCAL_DIR/conf/config.php
-    #rm -r $LOCAL_DIR/typo3temp
     ;;
     "xt:commerce")
     # xt:commerce
@@ -107,6 +110,7 @@ case "$CMS" in
     sed -i "s/define('DB_SERVER_USERNAME', '$REMOTE_MYSQL_USER');/define('DB_SERVER_USERNAME', '$LOCAL_MYSQL_USER');/" $LOCAL_DIR/includes/configure.php
     sed -i "s/define('DB_DATABASE', '$REMOTE_MYSQL_DB');/define('DB_DATABASE', '$LOCAL_MYSQL_DB');/" $LOCAL_DIR/includes/configure.php
     sed -i "s/define('DB_SERVER', '$REMOTE_MYSQL_HOST');/define('DB_SERVER', '$LOCAL_MYSQL_HOST');/" $LOCAL_DIR/includes/configure.php
-    #rm -r $LOCAL_DIR/typo3temp
+    sed -i "s@define('DIR_FS_DOCUMENT_ROOT', '.*');@define('DIR_FS_DOCUMENT_ROOT', '$LOCAL_DIR/');@" $LOCAL_DIR/includes/configure.php
+    sed -i "s@define('DIR_FS_CATALOG', '.*');@define('DIR_FS_CATALOG', '$LOCAL_DIR/');@" $LOCAL_DIR/includes/configure.php
     ;;
 esac
